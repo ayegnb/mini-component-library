fed1769b6e5d9bae42fb4d1dd90eee65021aac72
ayegnb <aiya.yegenberdiyeva@nu.edu.kz>
2022-01-14 12:25:53 +0600

    add Progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,46 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const SIZES = {
+    small: {
+      '--border-radius': 4 + 'px',
+      '--height': 8 + 'px',
+    },
+    medium: {
+      '--border-radius': 4 + 'px',
+      '--height': 12 + 'px',
+    },
+    large: {
+      '--border-radius': 8 + 'px',
+      '--height': 24 + 'px',
+    },
+  };
+
+  return (
+  <>
+    <VisuallyHidden>
+      <span>
+        Progress bar
+      </span>
+    </VisuallyHidden>
+    <Wrapper style={SIZES[size]}>
+        <Progress role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" />
+    </Wrapper>
+  </>);
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--border-radius);
+`;
+
+const Progress = styled.div`
+  width: ${props => props["aria-valuenow"] + '%'};
+  height: 100%;
+  background-color: ${COLORS.primary};
+  border-radius: ${props => props["aria-valuenow"] === '100' ? 'var(--border-radius)' : 'var(--border-radius) 0 0 var(--border-radius)'};
+`;
 
 export default ProgressBar;
