988be0814e71356c058497c9dffbd5c3518bdc10
ayegnb <aiya.yegenberdiyeva@nu.edu.kz>
2022-01-14 14:03:25 +0600

    fix styles in Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,24 +9,63 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <SelectWrapper value={value} onChange={onChange}>
-      {children}
-    </SelectWrapper>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
 
-const SelectWrapper = styled.select`
-  border: none;
-  background-color: ${COLORS.transparentGray15};
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  padding: 12px 16px;
+  padding-right: 52px;
   font-size: ${16 / 16}rem;
   color: ${COLORS.gray700};
-  padding: 12px;
+  background-color: ${COLORS.transparentGray15};
   border-radius: 8px;
-  width: min-content;
 
-  &:hover {
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color; 
+  }
+  
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  height: var(--size);
+  width: var(--size);
+  pointer-events: none;
 `;
 
 export default Select;
