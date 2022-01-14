2ebb27d5486fff3c85b2252476e819825256e116
ayegnb <aiya.yegenberdiyeva@nu.edu.kz>
2022-01-14 13:04:50 +0600

    fixed styles in Progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,47 +5,63 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  const SIZES = {
-    small: {
-      '--border-radius': 4 + 'px',
-      '--height': 8 + 'px',
-    },
-    medium: {
-      '--border-radius': 4 + 'px',
-      '--height': 12 + 'px',
-    },
-    large: {
-      '--border-radius': 8 + 'px',
-      '--height': 24 + 'px',
-    },
-  };
+const STYLES = {
+  small: {
+    borderRadius: 4,
+    height: 8,
+    padding: 0,
+  },
+  medium: {
+    borderRadius: 4,
+    height: 12,
+    padding: 0,
+  },
+  large: {
+    borderRadius: 8,
+    height: 24,
+    padding: 4,
+  },
+};
 
+const ProgressBar = ({ value, size }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknow size passed to ProgressBar: ${size}`);
+  }
   return (
-  <>
-    <VisuallyHidden>
-      <span>
-        Progress bar
-      </span>
-    </VisuallyHidden>
-    <Wrapper style={SIZES[size]}>
-        <Progress role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" />
-    </Wrapper>
-  </>);
+  <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={{
+    '--padding': styles.padding + 'px',
+    '--border-radius': styles.borderRadius + 'px',
+  }}>
+    <VisuallyHidden>{value}%</VisuallyHidden>
+    <BarWrapper>
+      <Bar style={{ 
+        '--width': value + '%',
+        '--height': styles.height + 'px',
+      }} />
+    </BarWrapper>
+  </Wrapper>);
 };
 
 const Wrapper = styled.div`
-  width: 370px;
-  height: var(--height);
+  background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: var(--border-radius);
+  padding: var(--padding);
 `;
 
-const Progress = styled.div`
-  width: ${props => props["aria-valuenow"] + '%'};
-  height: 100%;
+const BarWrapper = styled.div`
+  border-radius: var(--border-radius);
+  /* trim off corners when the progress bar is near-full */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
   background-color: ${COLORS.primary};
-  border-radius: ${props => props["aria-valuenow"] === '100' ? 'var(--border-radius)' : 'var(--border-radius) 0 0 var(--border-radius)'};
+  border-radius: var(--border-radius) 0 0 var(--border-radius);
 `;
 
 export default ProgressBar;
